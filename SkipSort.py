f188c1ec0299d0b586da382fcd247ae09e5fb4e1
llff123 <310813208@qq.com>
2020-03-29 13:35:22 +0800

    快排算法

---
+++ SkipSort.py
@@ -0,0 +1,29 @@
+def skip_sort(list,start,end):
+    '''
+    接收一个列表和第一个索引以及最后一个索引
+    1.将列表的第一个值定位第一次循环的基准值，大于基准值的排在右边，小于基准值的排在左边
+    2.先从右开始往左遍历，直到找到一个小于基准值的，将其赋值给基准值
+    3.再从左开始往右遍历，知道找到一个大于基准值的，将其赋值给第一步的值
+    4.如此循环，直到左指针大于等于右指针时，循环终止，此时的值成为新基准值
+    5.迭代循环
+    '''
+    if start >= end:
+        return
+    low = start
+    high = end
+    mid = list[start]
+    while low < high:
+        while low < high and list[high] >= mid:
+            high -= 1
+        list[low] = list[high]
+        while low < high and list[low] <mid:
+            low += 1
+        list[high] = list[low]
+    list[low] = mid
+    skip_sort(list,0,low-1)
+    skip_sort(list,low+1,end)
+
+if __name__ == '__main__':
+    list = [4,4,435,6,2,5,73,4,7,9,3,1,2]
+    skip_sort(list,0,len(list)-1)
+    print(list)
